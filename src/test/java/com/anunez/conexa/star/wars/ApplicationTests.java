ac4e1ad1ddc0c551a5538ac577fe2381fe47a920
Adam <adamaguirre96@gmail.com>
2025-05-01 21:29:39 -0300

    init commit

---
+++ src/test/java/com/anunez/conexa/star/wars/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anunez.conexa.star.wars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
